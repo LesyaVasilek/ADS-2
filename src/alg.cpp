22f771463a2d08ff9853adb33be50f900250c778
LesyaVasilek <125215125+LesyaVasilek@users.noreply.github.com>
2023-03-13 22:19:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,17 +3,17 @@
 
 
 double pown(double value, uint16_t n) {
-  double q = 1;
+  double res = 1;
   for (uint16_t i = 1; i <= n; i++)
-    q *= value;
-  return q;
+    res *= value;
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-  uint64_t result = 1;
+  uint64_t res = 1;
   for (uint64_t i = 2; i <= n; i++)
-    result *= i;
-  return result;
+    res *= i;
+  return res;
 }
 double calcItem(double x, uint16_t n) {
   return pown(x, n) / fact(n);
@@ -27,15 +27,15 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
-  double sinus = 0;
+  double res = 0;
   for (uint16_t i = 1; i <= count; i++)
-      sinus += pown(-1, i - 1) * pown(x, 2 * i - 1) / fact(2 * i - 1);
-  return sinus;
+      res += pown(-1, i - 1) * pown(x, 2 * i - 1) / fact(2 * i - 1);
+  return res;
 }
 
 double cosn(double x, uint16_t count) {
-    double cos = 0;
+    double res = 0;
     for (int i = 1; i <= count; i++)
-        cos += pown(-1, i - 1) * pown(x, 2 * i - 2) / fact(2 * i - 2);
-    return cos;
+        res += pown(-1, i - 1) * pown(x, 2 * i - 2) / fact(2 * i - 2);
+    return res;
 }
